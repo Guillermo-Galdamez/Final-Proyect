2e1e04ba95780d73385d3f17bd00804af02917cb
Guillermo-Galdamez <00084320@uca.edu.sv>
2021-11-30 18:39:26 -0600

    copiando proyecto

---
+++ ProyectoFinal/src/App.jsx
@@ -0,0 +1,25 @@
+import Login from "./pages/Login/Login";
+import Admin from "./pages/Admin/Admin";
+import Profile from "./pages/Profile/Profile";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>}  />
+            
+          <Route exact path="/admin" element={<Admin/>} />
+
+          <Route exact path="/profile" element={<Profile/>} />
+        </Routes>
+    </Router>  
+    </div> 
+  );
+}
+
+export default App;
---
+++ ProyectoFinal/src/Components/Post/Comment/Comment.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import { useForm } from 'react-hook-form'
+import SendIcon from '@mui/icons-material/Send';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import { useUserContext } from "../../../Contexts/UserContext";
+import { useUserServices } from "../../../Services/PageServices/User.services";
+const Comment = ({comments, id}) => {
+    const context = useUserContext();
+    const {register, errors, handleSubmit} = useForm();
+    const [send,setSend] = useState(<SendOutlinedIcon className="text-purple-900" fontSize="large"/>)
+
+
+    const onSubmitHandler = async (data, e) =>{
+        console.log(data);
+        const token = context.getToken();
+        console.log(token)
+        const response = await useUserServices.addComment(data.description, id ,token) 
+        console.log(response)
+        console.log(response.data)
+        e.target.reset();
+    }
+    
+    const onMouseEnterLikeHandler = () => {
+        setSend(<SendIcon className="text-purple-900" fontSize="large"/>)
+    } 
+
+    const onMouseLeaveLikeHandler = () => {
+        setSend(<SendOutlinedIcon  className="text-purple-900" fontSize="large"/>)
+    }  
+
+    return (
+        <div>
+            <div className="w-full h-20 overflow-auto p-2 pt-2  bg-red-400">{
+                  comments.map( comment => {
+                  return <p>{`${comment.user.username}: ${comment.description}`}</p>
+                })
+                }</div>
+            <form onSubmit={handleSubmit(onSubmitHandler)} className="w-full h-16 flex flex-row justify-between items-center ">
+                <input name="comment" className="w-4/5 h-8 ml-6 px-4 rounded-full border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Tengo que decir ..."  {...register("description")}/>
+                <button type="submit" onMouseEnter={onMouseEnterLikeHandler} onMouseLeave={onMouseLeaveLikeHandler} className="mr-6 " id="send">{send}</button>
+            </form>
+        </div>
+        
+    );
+};
+
+export default Comment;
